e9810b006de7beb73aae37491114cb67c1d7b5da
Swarnika Jaiswal <swarnikajaiswal584@gmail.com>
2026-07-17 12:18:46 +0530

    Prepare backend for deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,16 +3,10 @@ const aiRoutes = require('./routes/ai.routes')
 const cors = require('cors')
 
 
-
-
-
 const app = express();
 
 app.use(cors())
-
-
 app.use(express.json());
-
 
 
 app.get('/', (req, res)=> {
